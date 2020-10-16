a8636a1762016a6c3f4c3e2bd8e74a4868df865e
Los Vitaly <38357863+vitaly-los@users.noreply.github.com>
2020-10-16 22:02:54 +0300

    Update diff.py

--- diff.py
+++ diff.py
@@ -5,3 +5,18 @@ r = requests.get('https://imgs.xkcd.com/comics/python.png')
 
 with open('comic.png', 'wb') as f:
   f.write(r.content)
+
+
+ # find all links on page
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://site.ua'
+
+req = requests.get(url)
+
+soup = BeautifulSoup(req.text, 'lxml')
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
